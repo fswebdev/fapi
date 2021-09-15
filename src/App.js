4f90c563fbb05289705988f0126c953769fdc963
fswebdev <47744494+fswebdev@users.noreply.github.com>
2021-09-16 01:46:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import url("https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css");
+import url('https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css');
 import axios from "axios";
 import "./App.css";
 
